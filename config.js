97de29eef15b60e86bafd47b016081e1eeda57e9
UserProger <166346529+UserProger@users.noreply.github.com>
2024-10-03 17:55:01 +0500

    some
    some

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "12e385dfe552605244ed8e376df392275da356b4c496cf178940aad3f607f252b29b0abf637a3feb74fbc93cc80fbbe32a9ba8d2790adb1c3607aafd590ee68cdac3739b15456e7ff7e1d93c6680d475d97240f1b4f6435971316072f9bc2ed14e20b89119d19346fe4aa79963f51b66473992faabf52fa3de56672fb96a3855c269d7b58bf8e9a33cffae2d6010c5445089377543c48fd3393b922d729856d4f4cf53535ca4b73471ab7142e67fec1bac641cbf6bd67736982bb689d34486703de370ae991bfe7845e28f54e9f752b939b6b121ebf64e0ff11b85c20a3bd24d6101f8d07ce4ce160b866de17648bdb0c494d5f480fb8f561a772ccdf5171870",
+    EMAIL: "steve.franecki@ethereal.email", // testing email & password
+    PASSWORD : "sMf46xCzrvdrxvuagc",
+    ATLAS_URI: "mongodb+srv://arlen:7667assa@cluster0.z3rmk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+}
